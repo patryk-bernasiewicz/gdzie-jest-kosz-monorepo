b9715e97f5d96e88c1114a25758f4c71e7fe7af5
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2025-04-14 22:36:05 +0200

    fix error message for legal field on sign up

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -94,7 +94,7 @@ export default function SignUpScreen() {
         );
         const legalErrorText =
           errorTranslations[legalError?.code || ""] ||
-          passwordError?.message ||
+          legalError?.message ||
           null;
 
         setErrors({
