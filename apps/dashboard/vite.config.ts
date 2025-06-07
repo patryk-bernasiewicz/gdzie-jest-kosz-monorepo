08858f377fdf8acc1579d88afdcf575915070555
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2025-06-08 01:32:11 +0200

    fix shared packages access within apps

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3223,
+  },
+  resolve: {
+    alias: {
+      "@gjk/shared-utils": path.resolve(__dirname, "../../packages/shared-utils/src/index.ts"),
+    },
   },
 });
---
+++ packages/shared-utils/src/index.ts
@@ -0,0 +1,1 @@
+export { default as testUtil } from './test-util';
