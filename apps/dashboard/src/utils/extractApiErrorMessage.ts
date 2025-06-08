b263af08549cb7ece2c290b4902da17b61baed2a
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2025-06-08 04:54:55 +0200

    add missing prettier ignore file, run format on dashboard files

--- apps/dashboard/src/utils/extractApiErrorMessage.ts
+++ apps/dashboard/src/utils/extractApiErrorMessage.ts
@@ -1,16 +1,16 @@
 export function extractApiErrorMessage(error: unknown): string | null {
   if (
-    typeof error === 'object' &&
+    typeof error === "object" &&
     error !== null &&
-    'response' in error &&
-    typeof (error as any).response === 'object' &&
+    "response" in error &&
+    typeof (error as any).response === "object" &&
     (error as any).response !== null &&
-    'data' in (error as any).response &&
-    typeof (error as any).response.data === 'object' &&
+    "data" in (error as any).response &&
+    typeof (error as any).response.data === "object" &&
     (error as any).response.data !== null &&
-    typeof (error as any).response.data.message === 'string'
+    typeof (error as any).response.data.message === "string"
   ) {
     return (error as any).response.data.message;
   }
   return null;
-} 
+}
