755efabcd9502387e4316f4bf226390b0a19daa2
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2025-06-08 04:57:01 +0200

    [DEV-6] improve function extracting error messages from api responses

--- apps/dashboard/src/utils/extractApiErrorMessage.ts
+++ apps/dashboard/src/utils/extractApiErrorMessage.ts
@@ -1,16 +1,45 @@
+interface ErrorWithMessage {
+  message: string;
+}
+
+interface ErrorWithData {
+  data: ErrorWithMessage;
+}
+
+interface ErrorWithResponse {
+  response: ErrorWithData;
+}
+
+function isErrorWithMessage(obj: unknown): obj is ErrorWithMessage {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'message' in obj &&
+    typeof (obj as { message: unknown }).message === 'string'
+  );
+}
+
+function isErrorWithData(obj: unknown): obj is ErrorWithData {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'data' in obj &&
+    isErrorWithMessage((obj as { data: unknown }).data)
+  );
+}
+
+function isErrorWithResponse(obj: unknown): obj is ErrorWithResponse {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'response' in obj &&
+    isErrorWithData((obj as { response: unknown }).response)
+  );
+}
+
 export function extractApiErrorMessage(error: unknown): string | null {
-  if (
-    typeof error === "object" &&
-    error !== null &&
-    "response" in error &&
-    typeof (error as any).response === "object" &&
-    (error as any).response !== null &&
-    "data" in (error as any).response &&
-    typeof (error as any).response.data === "object" &&
-    (error as any).response.data !== null &&
-    typeof (error as any).response.data.message === "string"
-  ) {
-    return (error as any).response.data.message;
+  if (isErrorWithResponse(error)) {
+    return error.response.data.message;
   }
   return null;
 }
