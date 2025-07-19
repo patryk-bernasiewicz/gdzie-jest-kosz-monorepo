947271a264a29ccb7b564d9da9b1fb02ba2ddc74
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2025-07-19 16:39:29 +0200

    [DEV-29] remove all files related to map view

--- apps/mobile/feature/map/utils/parts/handleMapLoadHtml.ts
+++
@@ -1,9 +0,0 @@
-export const handleMapLoadHtml = /* js */ `
-  // -- handle map onload event
-  map.on('load', function(event) {
-    loggerInstance.log('Map loaded', event);
-    window.ReactNativeWebView.postMessage(JSON.stringify({
-      type: 'maploaded',
-    }));
-  });
-`;
--- apps/mobile/feature/map/utils/parts/updateMapPositionHtml.ts
+++
@@ -1,8 +0,0 @@
-export const updateMapPositionHtml = /* js */ `
-  // -- Update position
-  window.updateMapPosition = function(lat, lng) {
-    //loggerInstance.log('Map position updated to: ' + lat + ', ' + lng);
-    map.setView([lat, lng], map.getZoom());
-    userMarker.setLatLng([lat, lng]);
-  };
-`;
